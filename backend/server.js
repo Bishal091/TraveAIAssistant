6b27c5d5f089c9cd3d757b8c790d77210b698e88
Bishal Singh <72182666+Bishal091@users.noreply.github.com>
2025-01-12 22:27:20 +0530

    logout fix1

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,9 @@ app.use(cors({
   origin: 'https://traveai.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'] 
 }));
+
 app.use(express.json());
 
 
